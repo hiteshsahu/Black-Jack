8eb2e572b9dc5651df008398b6a29cf4fffe21a5
Hitesh Sahu <hiteshkrsahu@gmail.com>
2017-12-29 10:25:11 +0530

    translated to kotlin

---
+++ app/src/androidTest/java/game_of_cards/hiteshsahu/com/blackjack/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package game_of_cards.hiteshsahu.com.blackjack
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("game_of_cards.hiteshsahu.com.blackjack", appContext.packageName)
+    }
+}
